b4753eefbd219c2d6e8c127c91b9eb7733b00f04
Nadia Uwase <Nuwase17@alustudent.com>
2021-12-13 12:57:13 +0200

    req and setup modif

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"nuwase17@alustudent.com\"\n\
+echo "
+[general]n
+email = "nuwase17@alustudent.com"n
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "
+[server]n
+headless = truen
+enableCORS=falsen
+port = $PORTn
 " > ~/.streamlit/config.toml
